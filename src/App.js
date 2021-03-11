dd692e3cb820294ae166aa3e3026d53a9e65de3d
Sam Li <xysamrin0@gmail.com>
2021-03-11 11:54:02 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+        <Container>
+            <Row>
+          <Col>1 of 1</Col>
+        </Row>
+        </Container>
+    </div>
+  );
+}
+
+export default App;
